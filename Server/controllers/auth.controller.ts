19876b31a2d881fbd1e66e346b251a3f500caf08
Dev-Tuhin01 <tuhinshubhrachakraborty@gmail.com>
2024-07-08 11:08:01 +0530

    Finished Signin

--- Server/controllers/auth.controller.ts
+++ Server/controllers/auth.controller.ts
@@ -1,4 +1,5 @@
-import {Request,Response} from "express"
+import {Request,Response} from "express";
+import bcrypt from "bcryptjs"
 
 import { Teacher, Student } from "../models/user.model";
 import { error } from "console";
@@ -27,7 +28,6 @@ export const studSignup = async (req:Request,res:Response)=>{
       rollNo,
       gender} = req.body;
 
-    console.log(studentName,email);
     
     const student = await Student.findOne({studentName});
 
@@ -36,6 +36,9 @@ export const studSignup = async (req:Request,res:Response)=>{
     }
 
     //Pasword hashing
+    //generate salt
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
 
     //creating a default profilePic
     //https://avatar.iran.liara.run/username?username=[firstname+lastname]
@@ -47,7 +50,7 @@ export const studSignup = async (req:Request,res:Response)=>{
     const newStudent = new Student({
       studentName,
       email,
-      password,
+      password:hashedPassword,
       subject,
       semester,
       admissionYear,
@@ -56,25 +59,95 @@ export const studSignup = async (req:Request,res:Response)=>{
       profilePic
     });
 
-    await newStudent.save();
+    
+    if (newStudent) {
+      
+      await newStudent.save();
+      
+      res.status(201).json({
+        _id:newStudent._id,
+        studentName:newStudent.studentName,
+        rollNo:newStudent.rollNo,
+        password:newStudent.password,
+        profilePic:newStudent.profilePic
+      })
+    } else {
+      res.status(400).json({error:"Oops! Some data you sent is not right"});
+    }
 
-    res.status(201).json({
-      _id:newStudent._id,
-      studentName:newStudent.studentName,
-      rollNo:newStudent.rollNo,
-      profilePic:newStudent.profilePic
-    })
 
-  } catch (error) {
+     } catch (error) {
     console.error((error as Error).message);
     
-    res.status(500).json({error:"Internal Server Error"});
+    res.status(500).json({error:"Something is cooked in our machines! Don't worry"});
   } 
 }
 
 
 export const teacherSignup = async (req:Request,res:Response)=>{
-  const {} = req.body
+  try {
+    const {
+      teacherName,
+      email,
+      password,
+      subject,
+      joiningYear,
+      teacherID,
+      gender
+    } = req.body;
+    
+    const teacher = await Teacher.findOne({teacherName});
+
+    if(teacher){
+      return res.status(400).json({error:"You can only teach one class at a time proffessor"});
+    }
+
+
+    //Pasword hashing
+    //generate salt
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+
+    //generating profile pic
+    const teacherNameRef= teacherName.split(' ').join('+');
+    const profilePic = `https://avatar.iran.liara.run/username?username=${teacherNameRef}`;
+
+
+    //Saving the data to db
+
+    const newTeacher = new Teacher({
+      teacherName,
+      email,
+      password:hashedPassword,
+      subject,
+      joiningYear,
+      teacherID,
+      gender,
+      profilePic
+    });
+
+    if (newTeacher) {
+      await newTeacher.save();
+
+      res.status(201).json({
+        _id:newTeacher._id,
+        teacherID:newTeacher.teacherID,
+        teacherName:newTeacher.teacherName,
+        password:newTeacher.password,
+        profilePic:newTeacher.profilePic
+      })
+
+
+    } else {
+      
+    }
+
+
+  } catch (error) {
+    console.error((error as Error).message);
+
+    res.status(500).json({error:"Something is cooked in our machines! Don't worry"});   
+   }
 }
 
 
