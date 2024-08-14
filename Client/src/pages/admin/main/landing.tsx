7e370746724a6db066c34a688f86e2996b1f6c41
Dev-Tuhin01 <tuhinshubhrachakraborty2002@gmail.com>
2024-08-14 16:40:10 +0530

    fixed buttons in admin panel

--- Client/src/pages/admin/main/landing.tsx
+++ Client/src/pages/admin/main/landing.tsx
@@ -9,11 +9,11 @@ const Landing= () =>{
     <h3 className="text-secondary md:text-7xl text-3xl"> Department of CIS</h3>
     <h6 className="text-info">Admin panel, only for authorised personels</h6>
     <section className="divider divider-neutral"></section>
-    <nav className="grid grid-cols-2 gap-4 h-fit w-full justify-items-center grid-rows-2">
-        <div className="w-11/12 mx-96 h-24 text-white flex items-center justify-center text-xl bg-secondary text-black rounded"> <Link to={"studentEnrollment"}>Student Enrollment</Link> </div>
-        <div className="w-11/12 mx-96 h-24 text-white flex items-center justify-center text-xl bg-error rounded"><Link to={"teacherEnrollment"}> Teacher Enrollment</Link></div>
-        <div className="w-11/12 mx-96 h-24 text-white flex items-center justify-center text-xl bg-neutral rounded"><Link to={"publishNotice"}> Publish Notice</Link></div>
-        <div className="w-11/12 mx-96 h-24 text-white flex items-center justify-center text-xl bg-accent text-black rounded"><Link to={"addPaper"}> Add Paper</Link> </div>
+    <nav className="grid grid-cols-2 gap-4 h-full w-full justify-items-center grid-rows-2">
+        <Link to={"studentEnrollment"} className="w-11/12"><div className="h-24 text-white flex items-center justify-center text-xl bg-secondary text-black rounded"> Student Enrollment </div></Link>
+        <Link to={"teacherEnrollment"} className="w-11/12"><div className="h-24 text-white flex items-center justify-center text-xl bg-error rounded"> Teacher Enrollment</div></Link>
+        <Link to={"publishNotice"} className="w-11/12"><div className="h-24 text-white flex items-center justify-center text-xl bg-neutral rounded"> Publish Notice</div></Link>
+        <Link to={"addPaper"} className="w-11/12"><div className="h-24 text-white flex items-center justify-center text-xl bg-accent text-black rounded"> Add Paper </div></Link>
     </nav>
   </div>
   )
