0467d81f9e2592f5883ce31dba5df130765fa931
Dev-Tuhin01 <tuhinshubhrachakraborty2002@gmail.com>
2024-08-29 02:47:55 +0530

    added functionality to show notice body

--- Client/src/pages/ user/notice/notice.tsx
+++ Client/src/pages/ user/notice/notice.tsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link as div } from "react-router-dom";
 import { useNoticeContext } from "../../../context/noticeContext";
+import { title } from "process";
 
 const Notice =(props) =>{
 
-  const {noticeTitle,noticeCreatedby,createdAt,_id} = props.notice;
-  console.log(props.notice)
+  const {noticeTitle,noticeCreatedby,createdAt,_id,message} = props.notice;
+  console.log(_id)
   const {setNotice} = useNoticeContext()
 
   // Formatting the time from createdAt if available
@@ -19,13 +20,25 @@ const Notice =(props) =>{
 
   return (
     <div className="w-full min-h-2 p-4 border border-black rounded-xl">
-      <Link to={_id} onClick={setNotice(props.notice)}>
-      <h3 className="text-3xl">{noticeTitle}</h3>
-      <div className="flex justify-between">
-        <h5 className="text-xl">{noticeCreatedby}</h5>
-        <h5 className="text-xl">{time}</h5>
+      <div onClick={()=>document.getElementById(`${_id}`).showModal()} >
+        <h3 className="text-3xl">{noticeTitle}</h3>
+        <div className="flex justify-between">
+          <h5 className="text-xl">{noticeCreatedby}</h5>
+          <h5 className="text-xl">{time}</h5>
+        </div>
       </div>
-      </Link>
+      <dialog id={_id} className="modal">
+  <div className="modal-box">
+    <h3 className="font-bold text-lg">{noticeTitle}</h3>
+    <p className="py-4">{message} </p>
+    <div className="modal-action">
+      <form method="dialog">
+        {/* if there is a button in form, it will close the modal */}
+        <button className="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
     </div>
   )
 }
